88e4b0170e63580d53036c250727960d8e262819
aliadelabc <76475054+aliadelabc@users.noreply.github.com>
2022-07-22 20:23:11 +0200

    updated test

--- frontend/src/Summery.test.js
+++ frontend/src/Summery.test.js
@@ -1,11 +1,12 @@
-import { render, screen } from "@testing-library/react";
 //testing sending data
 test("Sending score to server to get rank%", async () => {
   const body = {
     score: "60",
   };
   try {
-    await request(app).post("http://localhost:89/api/v1/fetch-rank").send(body);
+    await request(app)
+      .post("http://localhost:4000/api/v1/fetch-rank")
+      .send(body);
   } catch (err) {
     // write test for failure here
     console.log(`Error ${err}`);
